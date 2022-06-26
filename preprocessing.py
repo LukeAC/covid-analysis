2cde19143ee3ac9b8b020b9e5fe1ae6c129f0ca0
Luke Collins <luke.a.collins@outlook.com>
2022-06-25 20:12:49 -0700

    feat: add attribute arg to preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
 
-def preprocessing(data: pd.DataFrame, threshold: int = None) -> pd.DataFrame:
-    data["Date"] = pd.to_datetime(data["Date"])
-    data = data.sort_values(by="Date", ascending=True)
+def preprocessing(
+    df: pd.DataFrame, threshold: int = None, attribute: str = "Confirmed"
+) -> pd.DataFrame:
+    df["Date"] = pd.to_datetime(df["Date"])
+    df = df.sort_values(by="Date", ascending=True)
     if threshold:
-        data = data.loc[data["Confirmed"] >= threshold]
-        data["days_since_threshold_crossed"] = data.reset_index().index
+        df = df.loc[df[attribute] >= threshold]
+        df["days_since_threshold_crossed"] = df.reset_index().index
 
-    return data
+    return df
